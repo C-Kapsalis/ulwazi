a0dd3427f049f036403d94cee3f8833ade182a17
Geoffrey Nyaga <geoffreynyagak@gmail.com>
2026-08-31 15:30:37 +0300

    refactor: remove sphinx-tabs support (#125)
    * refactor: remove sphinx-tabs support and update documentation to reflect changes
    
    * refactor: remove sphinx-tabs package and update dependencies
    
    * docs: drop orphaned Sphinx design link target

--- docs/conf.py
+++ docs/conf.py
@@ -330,7 +330,6 @@ myst_enable_extensions = {"colon_fence", "deflist", "substitution", "tasklist"}
 #       - sphinx_copybutton
 #       - sphinx_design
 #       - sphinx_reredirects
-#       - sphinx_tabs.tabs
 #       - sphinxcontrib.jquery
 #       - sphinxext.opengraph
 #       - terminal-output
@@ -351,7 +350,6 @@ extensions = [
     "canonical_sphinx_config",
     "myst_parser",
     "sphinxcontrib.jquery",
-    "sphinx_tabs.tabs",
     "sphinx_design",
 ]
 
